9d94198e0e32d6b33a0aded79286da90f17f3a44
Eric Anderson <anderson.eda@gmail.com>
2019-06-04 18:04:25 -0400

    last merge fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,7 @@ import Header from './Header';
 
 const Home = () => (
   <div>
-<<<<<<< HEAD
       <Header />
-=======
-      Welcome Home
->>>>>>> f192b88ea7874401fbfd5491316a6f5e8cf4919f
   </div>
 );
 
