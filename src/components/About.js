b7690988d35ff2265a617ce26928d7036fa6754e
Eric Anderson <anderson.eda@gmail.com>
2019-06-19 18:19:16 -0400

    sitewide tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -12,17 +12,17 @@ const about = () => {
               <img src={portrait} className="portrait__image"></img>
               <p className="portrait__message">Welcome! A little about me...
 
-              Computers have always been a central part of my life ever since I was a kid building PC's with my father.
+              Computers have always been a central part of my life, ever since I was a kid building PC's with my father.
               When it was time to figure out what to study in college, I knew it needed to be something in front of a screen. Thus the majority of my 
               formal education was centered around 3D Animation and Visual Effects. While it was something I enjoyed and was very good at,
-              I had an opportunity to live in NYC and run a cafe in Manhattan, and I took it. I got to meet thousands of interesting people,
+              I had an opportunity to move to NYC and run a cafe in Manhattan, and I took it. I got to meet thousands of interesting people,
               and have regulars like Alec Baldwin and Emma Stone! 
               
               My then future wife moved to NYC after landing her first job as a full-stack developer,
               and she opened my eyes to the programming world.
-              I began to dabble with code after hours, more specifically I started teaching myself to use python + django, and 
+              I began to dabble with code after hours (python + django), and 
               got a taste of the complexity that I loved about 3D, plus the power of being able to push something live
-              to the web. This is something I could see myself really enjoying.
+              to the web.
               
               As our priorities changed and we looked towards the future, we decided to move to North Carolina (the land of cheaper rent), where I would be able to take time off
               to learn to be a developer. And here we are!    </p>
