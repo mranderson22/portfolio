91b4dfaf9e3e39d1047eaa019f6614bce60350a8
Eric Anderson <anderson.eda@gmail.com>
2019-06-26 18:48:08 -0400

    SDK

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
-import * as firebaseSDK from '../../firebaseSDK';
 
 var firebaseConfig = {
-  apiKey: firebaseSDK.FIREBASE_API_KEY,
-  authDomain: firebaseSDK.FIREBASE_AUTH_DOMAIN,
-  databaseURL: firebaseSDK.FIREBASE_DATABASE_URL,
-  projectId: firebaseSDK.FIREBASE_PROJECT_ID,
-  storageBucket: firebaseSDK.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: firebaseSDK.FIREBASE_MESSAGING_SENDER_ID,
-  appId: firebaseSDK.FIREBASE_APP_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
